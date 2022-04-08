d3eeb0b36a25ef45064b95a25b9cc447c39eb8dd
Medina Hasna Lucita <97207992+Medinahl28@users.noreply.github.com>
2022-04-09 01:16:12 +0700

    Create kitoFintan.java
    This code is one of an overriding OOP assignment,
    
    and it's the first part, before michaFintan and kinaiFintan

---
+++ kitoFintan.java
@@ -0,0 +1,47 @@
+public class kitoFintan {
+    public String familyName;
+    private String hairstyle;
+    private int age;
+    private String superpower;
+    private String eyecolor;
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public void setHairstyle(String hairstyle) {
+        this.hairstyle = hairstyle;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSuperpower(String superpower) {
+        this.superpower = superpower;
+    }
+
+    public void setEyecolor(String eyecolor) {
+        this.eyecolor = eyecolor;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public String getHairstyle() {
+        return hairstyle;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSuperpower() {
+        return superpower;
+    }
+
+    public String getEyecolor() {
+        return eyecolor;
+    }
+}
