4c3af6dd3095d99b92070e7dfde1ac0601dd8f79
Medina Hasna Lucita <97207992+Medinahl28@users.noreply.github.com>
2022-04-08 22:32:20 +0700

    Create militaryGun.java
    This is the subclass of the class 'Guns'

---
+++ militaryGun.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class militaryGun extends Guns{
+    private String gunModel1 = "Long Gun";
+    private String gunModel2 = "Rifle";
+    private String gunModel3 = "Shotgun";
+
+    public void setGunModel1(String gunModel1) {
+        this.gunModel1 = gunModel1;
+    }
+    public String getGunModel1() {
+        return gunModel1;
+    }
+
+    public void setGunModel2(String gunModel2) {
+        this.gunModel2 = gunModel2;
+    }
+    public String getGunModel2() {
+        return gunModel2;
+    }
+
+    public void setGunModel3(String gunModel3) {
+        this.gunModel3 = gunModel3;
+    }
+    public String getGunModel3() {
+        return gunModel3;
+    }
+}
